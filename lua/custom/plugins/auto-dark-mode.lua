2643058ef541626e8dabf54990e4fa9691f6e1cf
Aliaksandr Truš <alex.trush@proton.me>
2024-01-05 17:13:04 +0100

    Replace auto-dark-mode.nvim by vim-lumen
    auto-dark-mode.nvim has some issue thats brakes lualine on startup

--- lua/custom/plugins/auto-dark-mode.lua
+++ lua/custom/plugins/auto-dark-mode.lua
@@ -1,14 +1,3 @@
 return {
-  "f-person/auto-dark-mode.nvim",
-  config = {
-    update_interval = 1000,
-    set_dark_mode = function()
-      vim.api.nvim_set_option("background", "dark")
-      -- vim.cmd [[colorscheme tokyonight]]
-    end,
-    set_light_mode = function()
-      vim.api.nvim_set_option("background", "light")
-      -- vim.cmd [[colorscheme tokyonight-day]]
-    end,
-  },
+  'vimpostor/vim-lumen',
 }
