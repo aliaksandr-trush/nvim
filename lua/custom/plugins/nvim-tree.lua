ef39b3777d60aa025532d27fb0423a1571e7b494
Aliaksandr Truš <alex.trush@proton.me>
2024-01-05 17:25:56 +0100

    Move configs to files
    Cleanup init.lua file a bit.
    Also remove oil.lua plugin.

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -6,6 +6,24 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      hijack_cursor = true,
+      filters = {
+        git_clean = false,
+      },
+      actions = {
+        open_file = {
+          quit_on_open = true,
+        },
+      },
+    }
+
+    vim.keymap.set('n', '<leader>tt', require('nvim-tree.api').tree.toggle, { desc = 'Toggle TreeView' })
+    vim.keymap.set('n', '<leader>ff', function()
+      require('nvim-tree.api').tree.find_file({ focus = true, open = true })
+    end, { desc = '[F]ind [f]ile in TreeView' })
+
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
   end,
 }
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -42,6 +42,7 @@ return {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
         'codelldb',
+        'debugpy',
       },
     }
 
