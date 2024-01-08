c342f5aa8322f4c1220905e4bd2e1b560755b736
Aliaksandr Truš <alex.trush@proton.me>
2024-01-08 11:35:10 +0100

    Move colorschemes to separate file

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -6,7 +6,8 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      theme = 'catppuccin',
+      -- theme = 'catppuccin',
+      theme = 'auto',
       component_separators = '|',
       section_separators = '',
     },
